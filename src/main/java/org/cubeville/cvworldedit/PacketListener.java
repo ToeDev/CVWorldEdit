cfca38a48a04dba70f2be0afb4b4cde1a2713c70
ToeDev <73237279+ToeDev@users.noreply.github.com>
2022-01-17 14:51:07 -0500

    changes

--- src/main/java/org/cubeville/cvworldedit/PacketListener.java
+++ src/main/java/org/cubeville/cvworldedit/PacketListener.java
@@ -8,14 +8,12 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
-import com.comphenix.protocol.wrappers.WrappedBlockData;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -23,12 +21,65 @@ public class PacketListener implements Listener {
 
     public PacketListener(CVWorldEdit cvWorldEdit, ProtocolManager protocolManager) {
         protocolManager.addPacketListener(
-                new PacketAdapter(cvWorldEdit, ListenerPriority.NORMAL, PacketType.Play.Client.SET_CREATIVE_SLOT) {
+                new PacketAdapter(cvWorldEdit, ListenerPriority.HIGHEST, PacketType.Play.Server.SET_SLOT) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        PacketContainer packet = event.getPacket();
+                        if(packet.getItemModifier().readSafely(0).getType() == Material.AIR) return;
+
+                        StructureModifier<ItemStack> items = packet.getItemModifier();
+                        ItemStack stack = items.readSafely(0);
+                        ItemMeta meta = stack.getItemMeta();
+                        if(meta == null) return;
+
+                        Player player = event.getPlayer();
+                        if(meta.getLore() != null) {
+                            for(String lore : meta.getLore()) {
+                                if(lore.equalsIgnoreCase("(+NBT)")) {
+                                    ItemStack newStack = new ItemStack(Material.AIR);
+                                    items.write(0, newStack);
+                                    player.closeInventory();
+                                    player.sendMessage(ChatColor.RED + "The item '" + ChatColor.GOLD + stack.getType() + ChatColor.RED + "' has been removed due to NBT values that are not allowed on this server!");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+        );
+    }
+
+    /*public PacketListener(CVWorldEdit cvWorldEdit, ProtocolManager protocolManager) {
+        protocolManager.addPacketListener(
+                new PacketAdapter(cvWorldEdit, ListenerPriority.HIGHEST, PacketType.Play.Server.CLOSE_WINDOW) {
                     @Override
                     public void onPacketReceiving(PacketEvent event) {
-                        event.getPlayer().sendMessage("size is: " + event.getPacket().getItemModifier().readSafely(0).getItemMeta().);
+                        PacketContainer packet = event.getPacket();
+                        if(packet.getItemModifier().readSafely(0).getType() == Material.AIR) return;
 
-                        PacketContainer fakeExplosion = new PacketContainer(PacketType.Play.Server.EXPLOSION);
+                        StructureModifier<ItemStack> items = packet.getItemModifier();
+                        ItemStack stack = items.readSafely(0);
+                        ItemMeta meta = stack.getItemMeta();
+                        if(meta == null) return;
+
+                        Player player = event.getPlayer();
+                        if(meta.getLore() != null) {
+                            for(String lore : meta.getLore()) {
+                                if(lore.equalsIgnoreCase("(+NBT)")) {
+                                    ItemStack newStack = new ItemStack(Material.AIR);
+                                    items.write(0, newStack);
+                                    //player.updateInventory();
+                                    player.sendMessage(ChatColor.RED + "This item contains NBT values not allowed on the server. Item Removed: " + stack.getType());
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+        );
+    }*/
+
+    /*PacketContainer fakeExplosion = new PacketContainer(PacketType.Play.Server.EXPLOSION);
                         fakeExplosion.getDoubles().
                                 write(0, event.getPlayer().getLocation().getX()).
                                 write(1, event.getPlayer().getLocation().getY()).
@@ -38,9 +89,5 @@ public class PacketListener implements Listener {
                             protocolManager.sendServerPacket(event.getPlayer(), fakeExplosion);
                         } catch (InvocationTargetException e) {
                             throw new RuntimeException("Cannot send packet " + fakeExplosion, e);
-                        }
-                    }
-                }
-        );
-    }
+                        }*/
 }
